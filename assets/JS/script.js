46c2e84ce5f30936a210b0593c4ae03340c1ed53
Jack James <jacksontallent48@gmail.com>
2022-08-24 22:27:14 -0400

    Added wikipedia api, balldontlie api, player search functionality

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,25 @@
-var playerNum = 1;
-var theplayerName = "lebron james";
+//vvv unused currently vvv
+var playerArray = [];
+//vvv for testing getWikiExtract() vvv
+var theplayerName = ["Lebron", "James"];
+var wikiText;
+//^^^ Above are test variables.^^^
+//vvv Below are probably final variables.vvv
+var player1SearchButton = document.querySelector("#player1SearchButton");
+var player2SearchButton = document.querySelector("#player2SearchButton");
+var playerName1Input = document.querySelector("#player-name");
+var playerName2Input = document.querySelector("#player-name2");
+var player1SearchResults = document.querySelector("#player1searchResults");
+var player1SearchResults = document.querySelector("#player2searchResults");
 
 //wikipedia search player
-async function getWikiExtract(nameA){
-    console.log(nameA);
-    //Actual API call
-    async function getResponse(){
+async function getWikiExtract(nameA) {
+    async function getResponse() {
         var theresponse = await axios.get("https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=extracts&titles=" + nameA[0] + "%20" + nameA[1] + "&exintro=1&explaintext=1&origin=*");
         return theresponse.data.query.pages[0].extract;
     }
     try {
-        const result_1 = await getResponse();
-        console.log("result1= " + result_1);
+        var result_1 = await getResponse();
         return result_1;
     } catch (error) {
         console.log("There has been an error: " + error);
@@ -19,15 +27,71 @@ async function getWikiExtract(nameA){
     }
 }
 
-//unfinished
-function nameFixer(name) {
-    var nameA = name.toString().split(" ");
-    if(nameA.length < 2){alert("names must be 2 words")}
-    nameA[0] = nameA[0].charAt(0).toUpperCase() + nameA[0].slice(1);
-    nameA[1] = nameA[1].charAt(0).toUpperCase() + nameA[1].slice(1);
-    if(nameA[0] === "Lebron"){nameA[0] = "LeBron";}
-    if (nameA.length > 2) { alert("Must be less than 3.") }
-    return nameA;
+//Player search.
+async function fetchSearchedPlayerName(searchTerm) {
+    console.log(searchTerm);
+    async function getResponse() {
+        var theresponse = await axios.get("https://www.balldontlie.io/api/v1/players?search=" + searchTerm);
+        return theresponse.data.data;
+    }
+    try {
+        var result_2 = await getResponse()
+        return result_2;
+    } catch (error) {
+        console.log("There has been an error: " + error);
+        return null;
+    }
 }
-var nameA = nameFixer(theplayerName);
-console.log(getWikiExtract(nameA));
+//Places players in search boxes (unless only 1 player was returned from search) <- still have to do that part
+function searchArea(searchArray, playerNum) {
+    if (playerNum === 1) { 
+        player1SearchButton.style.display = "none";
+        var searchBox = document.querySelector("#player1searchResults");
+    }
+    else { 
+        var searchBox = document.querySelector("#player2searchResults"); 
+        player2SearchButton.style.display = "none";
+    }
+    for (i=0;i<searchArray.length; i++) {
+        var searchResults = document.createElement("p");
+        var buttons = document.createElement("button");
+        searchBox.appendChild(searchResults);
+        searchResults.textContent = searchArray[i].first_name + " " + searchArray[i].last_name;
+        searchResults.appendChild(buttons);
+        buttons.textContent = "Select";
+        buttons.setAttribute("id",'"' + searchArray[i].id + '"');
+        buttons.addEventListener("click", function(){
+            searchBox.innerHTML = "";
+            console.log(this.id + " was clicked");
+        })
+    }
+    searchBox.style.display = "inline-block";
+    var clearButton = document.createElement("button");
+    searchBox.insertBefore(clearButton, searchBox.firstChild);
+    clearButton.textContent ="Clear Search";
+    clearButton.addEventListener("click",function(){
+        searchBox.innerHTML = "";
+        if (playerNum === 1) {player1SearchButton.style.display = "block"}
+        if (playerNum === 2) {player2SearchButton.style.display = "block"}
+    })
+
+}
+
+player1SearchButton.addEventListener("click", async function () {
+    //    console.log(playerName1Input.value.toString());
+    searchArea(await fetchSearchedPlayerName(playerName1Input.value.toString()), 1);
+})
+player2SearchButton.addEventListener("click", async function () {
+    searchArea(await fetchSearchedPlayerName(playerName2Input.value.toString()), 2);
+})
+
+
+
+
+
+// //these are just for testing.
+// async function dothething() {
+//     var wikiText = await getWikiExtract(theplayerName);
+//     console.log(wikiText);
+// }
+// dothething();
