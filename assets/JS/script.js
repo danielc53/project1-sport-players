a1020796a7a32765505bf8b4805c14c80976b211
Jack James <jacksontallent48@gmail.com>
2022-09-01 11:40:49 -0400

    Added season select.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,6 +8,8 @@ var wikiText;
 var player1SearchButton = document.querySelector("#player1SearchButton");
 var player2SearchButton = document.querySelector("#player2SearchButton");
 var homeBtn = document.querySelector("#home-btn");
+var Player1ClearButton = document.querySelector("#player1ClearButton");
+var Player2ClearButton = document.querySelector("#player2ClearButton");
 
 
 //wikipedia search player
@@ -51,7 +53,7 @@ async function fetchSearchedPlayerName(searchTerm) {
 async function getPlayerInfo(playerID) {
   try{
     var theresponse = await axios.get("https://www.balldontlie.io/api/v1/players/" + playerID);
-  } catch {
+  } catch(error) {
     openModal(error);
     return null;
   }
@@ -76,12 +78,12 @@ async function fetchSelectedSeasonAverages(playerID, season) {
     var seasonAverages = await axios.get("https://www.balldontlie.io/api/v1/season_averages?season=" + season + "&player_ids[]=" + playerID);
     return seasonAverages;
     
-  } catch {
-    openModal(error);
+  } catch(error) {
+    openModal("There was an error fetching the season averages. Error: " + error);
     return null;
   }
 }
-//Places players in search boxes (unless only 1 player was returned from search) <- still have to do that part
+//Places players in search boxes (unless only 1 player was returned from search)
 function searchArea(searchArray, playerNum) {
   if (playerNum === 1) {
     var searchBox = document.querySelector("#player1searchResults");
@@ -100,17 +102,52 @@ function searchArea(searchArray, playerNum) {
     buttons.setAttribute("class", "spacing")
     buttons.addEventListener("click", function () {
       searchBox.innerHTML = "";
-      handlePlayerSelect(this.id,playerNum);
+      selectSeason(this.id,playerNum);
     });
   }
   searchBox.style.display = "inline-block";
 }
+  async function selectSeason (playerID, playerNum) {
+    var playerData = await getPlayerInfo(playerID);
+    var seasonNum = 2021;
+    if (playerNum === 1) {
+      var loadSeasonsB = document.querySelector("#player1dropdownB");
+    }
+    else {
+      var loadSeasonsB = document.querySelector("#player2dropdownB");
+    }
+  //Makes dropdown list to select season averages
+  loadSeasonsB.style.display = "inline-block";  
+  var dropdownBox = document.createElement("select");
+  dropdownBox.setAttribute("selected",seasonNum);
+  dropdownBox.setAttribute("id","player" + playerNum + "options");
+  for (i=2021; i>1978; i--) {
+    var options = document.createElement("option");
+    options.setAttribute("value", i);
+    options.innerHTML = i;
+    dropdownBox.appendChild(options);
+  }
+  loadSeasonsB.after(dropdownBox);
+  loadSeasonsB.addEventListener("click", function(){
+    if (playerNum === 1) {
+      dropdownBox.remove()
+      loadSeasonsB.style.display = "none";
+      handlePlayerSelect(playerID, 1, dropdownBox.value);
+    }
+    else {
+      dropdownBox.remove()
+      loadSeasonsB.style.display = "none";
+      handlePlayerSelect(playerID, 2, dropdownBox.value);
+    }
+    
+  })
+  }
 
 
 //TODO: fetch images from local json maybe
-async function handlePlayerSelect(playerID, playerNum) {
-  var seasonStats = await fetchSelectedSeasonAverages(playerID, "2021");
-  //console.log(seasonStats);
+async function handlePlayerSelect(playerID, playerNum, seasonNum) {
+  console.log("this function is being called");
+  var seasonStats = await fetchSelectedSeasonAverages(playerID, seasonNum);
    var playerInfo = await getPlayerInfo(playerID);
    if(playerNum === 1){ 
     var contentBox = document.querySelector("#player1searchResults");
@@ -121,6 +158,7 @@ async function handlePlayerSelect(playerID, playerNum) {
     var contentBox =document.querySelector("#player2searchResults");
     var wikiExtractEl = document.querySelector("#wiki-bio2");
     var stats = document.querySelector("#stats2");
+    var loadSeasonsB = document.querySelector("#player2dropdownB");
   }
    var wikiExtract = await fetchWikiExtract(playerInfo.first_name, playerInfo.last_name);
    if (wikiExtract.length === 0) {
@@ -133,23 +171,36 @@ async function handlePlayerSelect(playerID, playerNum) {
    var assist = document.createElement("p");
    var steals = document.createElement("p");
 
-   rebounds.innerHTML = seasonStats.data.data[0].reb + " rpg.";
-   assist.innerHTML = seasonStats.data.data[0].ast + " apg.";
-   steals.innerHTML = seasonStats.data.data[0].stl + " spg.";
-   points.innerHTML = seasonStats.data.data[0].pts + " ppg.";
-   playerNameEl.innerHTML = playerInfo.first_name + " " + playerInfo.last_name;
-   wikiExtractEl.innerHTML = wikiExtract;
-   teamNameEl.innerHTML = playerInfo.team.name;
-   stats.appendChild(points); 
-   stats.appendChild(rebounds);
-   stats.appendChild(assist);
-   stats.appendChild(steals);
-   contentBox.appendChild(playerNameEl);
-   contentBox.appendChild(teamNameEl);
-   stats.style.display = "block";
-   teamNameEl.style.display = "block";
-   playerNameEl.style.display = "inline-block";
-   wikiExtractEl.style.display = "inline-block";
+  if (seasonStats.data.data.length > 0){
+    rebounds.innerHTML = seasonStats.data.data[0].reb + " rpg.";
+    assist.innerHTML = seasonStats.data.data[0].ast + " apg.";
+    steals.innerHTML = seasonStats.data.data[0].stl + " spg.";
+    points.innerHTML = seasonStats.data.data[0].pts + " ppg.";
+
+    stats.appendChild(points); 
+    stats.appendChild(rebounds);
+    stats.appendChild(assist);
+    stats.appendChild(steals);
+
+    playerNameEl.innerHTML = playerInfo.first_name + " " + playerInfo.last_name;
+    wikiExtractEl.innerHTML = wikiExtract;
+    teamNameEl.innerHTML = playerInfo.team.name;
+
+    contentBox.appendChild(playerNameEl);
+    contentBox.appendChild(teamNameEl);
+
+    stats.style.display = "block";
+    teamNameEl.style.display = "block";
+    playerNameEl.style.display = "inline-block";
+    wikiExtractEl.style.display = "inline-block";
+  }
+  else{
+    openModal("There are no recorded games for Player " + playerNum + " during the " + seasonNum + " season.");
+  }
+   
+   
+  
+   
 }
 
 function openModal(modalText) {
@@ -179,7 +230,6 @@ function openModal(modalText) {
   player1SearchButton.style.display = "none";
   player1SearchResults.style.display = "block";
 
-  var Player1ClearButton = document.querySelector("#player1ClearButton");
   Player1ClearButton.style.display = "block";
   Player1ClearButton.addEventListener("click", function () {
       player1SearchResults.innerHTML = "";
@@ -193,7 +243,7 @@ function openModal(modalText) {
 
   var searchResults = await fetchSearchedPlayerName(playerName1Input.value.toString())
   if (searchResults.length === 1) {
-    handlePlayerSelect(searchResults[0].id, 1);
+    selectSeason(searchResults[0].id, 1);
   }
   else if (searchResults.length > 1) {
     searchArea(searchResults, 1);
@@ -204,7 +254,6 @@ function openModal(modalText) {
 });
 
 player2SearchButton.addEventListener("click", async function () {
-  var Player2ClearButton = document.querySelector("#player2ClearButton");
   var playerName2Input = document.querySelector("#player-name2");
   var stats = document.querySelector("#stats2");
   var player2SearchResults = document.querySelector("#player2searchResults");
@@ -212,6 +261,7 @@ player2SearchButton.addEventListener("click", async function () {
   player2SearchButton.style.display = "none";
   Player2ClearButton.style.display = "block";
   player2SearchResults.style.display = "block";
+  
   Player2ClearButton.addEventListener("click", function () {
       player2SearchResults.innerHTML = "";
       wikiBio2.innerHTML = "";
@@ -223,7 +273,7 @@ player2SearchButton.addEventListener("click", async function () {
     })
   var searchResults = await fetchSearchedPlayerName(playerName2Input.value.toString())
   if (searchResults.length === 1) {
-    handlePlayerSelect(await searchResults[0].id, 2);
+    selectSeason(await searchResults[0].id, 2);
   }
   else if (searchResults.length > 1){
     searchArea(searchResults, 2);
